bf6687bf8f868b8b766eac2b48d340a8751ce27a
bahae112 <bahaeaouanet2004@gmail.com>
2025-02-11 12:09:06 +0100

    Ajoutez des fichiers projet.

---
+++ WebApplication3/Controllers/ProduitController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ProduitController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Details()
+    {
+        return View();
+    }
+}
---
+++ WebApplication3/Models/Categorie.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication3.Models
+{
+    public class Categorie
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string OrdreAffichage { get; set; }
+    }
+}
