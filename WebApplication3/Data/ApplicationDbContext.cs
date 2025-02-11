81ee316011b5766dd8ca0a82ad76041a03eae92f
bahae112 <bahaeaouanet2004@gmail.com>
2025-02-11 19:28:19 +0100

    added

---
+++ WebApplication3/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication3.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Constructeur sans paramètre
+        public ApplicationDbContext()
+        {
+            // Initialisation ou logique spécifique
+        }
+    }
+}
